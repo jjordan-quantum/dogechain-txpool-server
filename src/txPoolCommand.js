96945ab27888ec46d1ae6adc8a7afdf2c3af40ce
Justin <justinjordantech@gmail.com>
2022-08-21 08:54:58 -0700

    added simple pruning

--- src/txPoolCommand.js
+++ src/txPoolCommand.js
@@ -1,8 +1,11 @@
 exports.TxPool = (function() {
     const { spawn } = require("child_process");
     require('dotenv').config();
-    const transactions = [];
-    const knownTransactions = [];
+    let transactions = [];
+    let knownTransactions = [];
+    let busy = false;
+    let lastPruning = Date.now();
+    const PRUNE_INTERVAL = process.env.GRPC_URL ? parseInt(process.env.PRUNE_INTERVAL) : 20*60*1000;
 
     async function subscribe() {
         const ls = spawn("dogechain", ["txpool", "subscribe", process.env.GRPC_URL || '127.0.0.1:9632']);
@@ -39,14 +42,37 @@ exports.TxPool = (function() {
                         if(token.startsWith('0x')) {
                             //console.log(token);
 
-                            if(!transactions.includes(token)) {
-                                transactions.push(token);
+                            if(!transactions.map((tx) => { return tx.hash; }).includes(token)) {
+                                transactions.push({hash: token, timestamp: Date.now()});
                             }
                         }
                     });
                 }
             })
         }
+
+        // default 20 min prune interval
+        const now = Date.now();
+
+        if(now > (lastPruning + PRUNE_INTERVAL)) {
+            pruneHashes(now);
+        }
+    }
+
+    function pruneHashes(now) {
+        busy = true;
+        const keepTransactions = [];
+
+        transactions.forEach((tx) => {
+           if(tx.timestamp > lastPruning && !knownTransactions.includes(tx.hash))  {
+               keepTransactions.push(tx);
+           }
+        });
+
+        transactions = keepTransactions;
+        knownTransactions = [];
+        lastPruning = now;
+        busy = false;
     }
 
     (async () => {
@@ -64,12 +90,16 @@ exports.TxPool = (function() {
     })();
 
     const getNewTransactions = () => {
+        if(busy) {
+            return [];
+        }
+
         const _transactions = [];
 
         transactions.forEach((tx) => {
-            if(!knownTransactions.includes(tx)) {
+            if(!knownTransactions.includes(tx.hash)) {
                 _transactions.push(tx);
-                knownTransactions.push(tx);
+                knownTransactions.push(tx.hash);
             }
         })
 
